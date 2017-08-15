d6dfcbad96c2b802846a62855bd670bd701b43a3
Connor Depies <connordepies@agro200953237.cfans.umn.edu>
2017-08-15 13:22:51 -0500

    Added R program to find means of column

--- scripts/LABELMATRIX.R
+++ scripts/LABELMATRIX.R
@@ -1,3 +1,4 @@
+# Connor Depies August, 15, 2017
 ##Takes the file containing the names of the samples and an ibs matrix and labels which two samples are being compared at each point in the matrix 
 wildintrogressions <- read.table(file="/Users/connordepies/WildIntrogression/data/plink.mibs", header = FALSE, sep = "\t")
 wildintrogresssions.names <- read.table(file = "/Users/connordepies/WildIntrogression/data/names.txt")
