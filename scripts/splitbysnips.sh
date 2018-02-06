3a35d8ee86066e99a76c7402b78edff1757f2ce3
Connor Depies <connordepies@agro200953237.cfans.umn.edu>
2018-02-06 09:55:09 -0600

    Added Comments

--- scripts/splitbysnips.sh
+++ scripts/splitbysnips.sh
@@ -15,7 +15,7 @@ getArray() {
 
 module load plink/1.90b
 vcf=$1
-working_directory=$2
+# Split the VCF file by chromosome
 awk '{print $1, $3}' $1 >chr.txt
 grep "chr1H" chr.txt >chr1.txt
 grep "chr2H" chr.txt >chr2.txt
@@ -31,18 +31,26 @@ awk '{print $2}' chr4.txt >chr4.list
 awk '{print $2}' chr5.txt >chr5.list
 awk '{print $2}' chr6.txt >chr6.list
 awk '{print $2}' chr7.txt >chr7.list
+# Make an array of the SNPs in each Chromosome
 chrarray=(chr1.list chr2.list chr3.list chr4.list chr5.list chr6.list chr7.list)
+# Iterate over this array
 for i in ${chrarray[@]};
 do
+    # Create an array containing SNP names from each Chromosome
     getArray $i
+     # Find IBD of each Chromosome
     plink --vcf $1 --extract ${i} --genome --out ${i}_out  --allow-extra-chr;
+    # Find length of the array
     len=${#array[@]}
+    # Create sliding windows each containing 100 SNPS from a chromosome, and moving by 10 SNPs each iteration
     for j in `seq 1 10 $len`;
     do
         if (("$j+100"<"$len"))
         then 
-            #source: https://stackoverflow.com/questions/20243467/write-bash-array-to-file-with-newlines
+            # source: https://stackoverflow.com/questions/20243467/write-bash-array-to-file-with-newlines
+            # Write each of these lists of 100 SNP windows to a file
             echo ${array[@]:$j:100} >${i}${j}_extract.txt
+            # Find IBD of this interval
             plink --vcf $1 --extract ${i}${j}_extract.txt --genome --out ${i}${j}_out --allow-extra-chr
         fi 
     done
