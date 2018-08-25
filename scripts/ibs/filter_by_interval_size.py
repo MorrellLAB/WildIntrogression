fa6eac9330142c0a3331a927d5c0298ef9372f3a
ChaochihL <liux1299@umn.edu>
2018-08-24 20:27:00 -0500

    account for overlapping windows

--- scripts/ibs/filter_by_interval_size.py
+++ scripts/ibs/filter_by_interval_size.py
@@ -4,7 +4,6 @@
 
 import sys
 
-
 def read_file(input_file, introg_dict):
     with open(input_file, 'r') as f:
         for line in f:
@@ -12,8 +11,8 @@ def read_file(input_file, introg_dict):
                 continue
             else:
                 tmp = line.strip().split('\t')
-                key_name = tmp[0] + '_' + tmp[1]    # ID1 and ID2 columns
-                rest = [tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]]
+                key_name = tmp[0] + '_' + tmp[1] + '_' + tmp[2] # ID1, ID2, and Chr columns
+                rest = [tmp[3], tmp[4], tmp[5], tmp[6]]
                 if key_name in introg_dict.keys():
                     introg_dict[key_name].append(rest)
                 else:
@@ -22,21 +21,81 @@ def read_file(input_file, introg_dict):
 
 
 def main(file_list, threshold):
-    d = {}
+    d = {} # Initialize empty dictionary
     read_file(input_file=file_list, introg_dict=d)
-
-    tmp_d = {}
+    #   combining overlapping intervals, so we can calculate
+    #   size of continuous interval
+    #   necessary b/c for same chr, some overlap and some don't
+    merged_d = {}
     for key in d.keys():
-        if len(d[key]) == 1 and int(d[key][0][4]) < int(threshold):
-            tmp_d[key] = d[key][0]
-
-    for key in tmp_d.keys():
-        d.pop(key, None)
-
-    for key in d.keys():
+        base = d[key][0]
+        base_index = 0
+        # Add base key value pair to merged_d
+        merged_win = [
+            base[0].split('-')[0], # win_start
+            base[0].split('-')[1], # win_end
+            base[1], # start_pos
+            base[2], # end_pos
+        ]
+        merged_d[key] = [merged_win]
         for i in range(0, len(d[key])):
-            print('\t'.join(key.split('_')), *d[key][i], sep='\t')
+            if len(d[key]) == 1:
+                merged_win = [
+                    base[0].split('-')[0], # win_start
+                    d[key][i][0].split('-')[1], # win_end
+                    base[1], # start_pos
+                    d[key][i][2], # end_pos
+                ]
+                merged_d[key] = [merged_win]
+            elif (i > base_index and int(d[key][i][0].split('-')[0]) < int(d[key][i-1][0].split('-')[1])):
+                # Values will contain: win_start, win_end, start_pos, end_pos
+                merged_win = [
+                    base[0].split('-')[0], # win_start
+                    d[key][i][0].split('-')[1], # win_end
+                    base[1], # start_pos
+                    d[key][i][2], # end_pos
+                ]
+                # Dictionary values are a list of lists
+                # Get last index of list of lists
+                # This works because, other condition appends a list to list of lists
+                # So, most current one we are updating will always be the nth list
+                # If dictionary is not empty, add by list index
+                if (key in merged_d.keys() and len(merged_d[key]) == 1):
+                    merged_d[key][0] = merged_win
+                elif (key in merged_d.keys() and len(merged_d[key]) > 1):
+                    last_list = len(merged_d[key]) - 1
+                    merged_d[key][last_list] = merged_win
+                else:
+                    merged_d[key] = [merged_win]
+            else:
+                if int(d[key][i][0].split('-')[0]) > int(d[key][i-1][0].split('-')[1]):
+                    # Update base case
+                    base = d[key][i]
+                    base_index = i
+                    # Values will contain: win_start, win_end, start_pos, end_pos
+                    merged_win = [
+                        base[0].split('-')[0], # win_start
+                        d[key][i][0].split('-')[1], # win_end
+                        base[1], # start_pos
+                        d[key][i][2], # end_pos
+                    ]
+                    merged_d[key].append(merged_win)
+
+    #   Calculate size of window
+    for key in merged_d.keys():
+        for i in range(0, len(merged_d[key])):
+            #   Get total physical size of window
+            phys_size = int(merged_d[key][i][3]) - int(merged_d[key][i][2])
+            merged_d[key][i].append(str(phys_size))
+
+    #   Print header line
+    print("Ind1\tInd2\tChr\tSNP_Win_Start\tSNP_Win_End\tPhysPos_Start\tPhysPos_End\tInt_Phys_Size")
+    #   Filter out interval sizes smaller than threshold
+    #   Keep only those above threshold
+    for key in merged_d.keys():
+        for i in range(0, len(merged_d[key])):
+            if int(merged_d[key][i][4]) >= int(threshold):
+                print('\t'.join(key.split('_')), *merged_d[key][i], sep='\t')
 
 
 main(sys.argv[1], sys.argv[2])
-
