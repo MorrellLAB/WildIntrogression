bed1bc2d78ee2caff578bd7c49e011056ef0b508
ChaochihL <liux1299@umn.edu>
2024-06-01 17:01:12 -0500

    add counts and combine with Table S3

--- analysis/per_sample_prop_genome_introgressed.R
+++ analysis/per_sample_prop_genome_introgressed.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(tidyverse)
+library(dplyr)
 library(readxl)
 
 setwd("~/Dropbox/Projects/Wild_Introgression/Analyses/local_ancestry-flare/wild-introgressed-visible_pheno_and_Azerbaijan")
@@ -26,9 +27,10 @@ vis_pheno_samples <- vis_pheno$Accession_ID
 df_tracts_vis_pheno <- df[df$sample %in% vis_pheno_samples, ]
 # Sum per sample introgressed segments
 df_prop_vis_pheno <- df_tracts_vis_pheno %>%
-    select(sample, bp_length, Mbp_length) %>%
+    dplyr::select(sample, bp_length, Mbp_length) %>%
     group_by(sample) %>%
-    dplyr::summarise(total_bp_length=sum(bp_length),
+    dplyr::summarise(num_introgressed_segments=n(),
+                     total_bp_length=sum(bp_length),
                      total_Mbp_length=sum(Mbp_length),
                      prop_genome_introgressed=sum(bp_length)/morex_v3_size_bp)
 df_prop_vis_pheno$description <- "Visible Phenotype"
@@ -40,14 +42,34 @@ azerbaijan_samples <- azerbaijan_df$Accession_ID
 df_tracts_azerbaijan <- df[df$sample %in% azerbaijan_samples, ]
 # Sum per sample introgressed segments
 df_prop_azerbaijan <- df_tracts_azerbaijan %>%
-    select(sample, bp_length, Mbp_length) %>%
+    dplyr::select(sample, bp_length, Mbp_length) %>%
     group_by(sample) %>%
-    dplyr::summarise(total_bp_length=sum(bp_length),
+    dplyr::summarise(num_introgressed_segments=n(),
+                     total_bp_length=sum(bp_length),
                      total_Mbp_length=sum(Mbp_length),
                      prop_genome_introgressed=sum(bp_length)/morex_v3_size_bp)
 df_prop_azerbaijan$description <- "Azerbaijan"
 
+# Wild-introgressed samples not with visible phenotype or from Azerbaijan
+df_remaining <- df[!(df$sample %in% c(vis_pheno_samples, azerbaijan_samples)), ]
+# Sum per sample introgressed segments
+df_prop_remaining <- df_remaining %>%
+    dplyr::select(sample, bp_length, Mbp_length) %>%
+    group_by(sample) %>%
+    dplyr::summarise(num_introgressed_segments=n(),
+                     total_bp_length=sum(bp_length),
+                     total_Mbp_length=sum(Mbp_length),
+                     prop_genome_introgressed=sum(bp_length)/morex_v3_size_bp)
+df_prop_remaining$description <- NA
+
 df_out <- rbind(df_prop_vis_pheno, df_prop_azerbaijan)
+df_all <- rbind(df_prop_vis_pheno, df_prop_azerbaijan, df_prop_remaining) %>%
+    arrange(desc(prop_genome_introgressed))
+
+sink(file="wild_introgressed_6number_summary-all.txt")
+summary(df_all$prop_genome_introgressed)
+sink()
 
 # Save to file
 write_csv(df_out, file="wild_introgressed_per_sample_prop-vis_pheno_and_Azerbaijan.csv", quote="none", col_names=TRUE)
+write_csv(df_all, file="wild_introgressed_per_sample_prop-all.csv", quote="none", col_names=TRUE, na="")
